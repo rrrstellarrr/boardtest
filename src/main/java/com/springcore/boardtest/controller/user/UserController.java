300562b4b93dafd72093432e6ab1ee0888e1e89c
stella <stella.is.a.starrr@gmail.com>
2022-07-06 17:35:34 +0900

    로그인 실패 체크 오류 수정

--- src/main/java/com/springcore/boardtest/controller/user/UserController.java
+++ src/main/java/com/springcore/boardtest/controller/user/UserController.java
@@ -32,7 +32,11 @@ public class UserController {
 
     // 회원 로그인 페이지
     @GetMapping("/login")
-    public String login() {
+    public String login(@RequestParam(value = "error", required = false)String error,
+                        @RequestParam(value = "exception", required = false)String exception,
+                        Model model) {
+        model.addAttribute("error", error);
+        model.addAttribute("exception", exception);
         return "/user/login";
     }
 
